2f63d4ced5a99542b25e7c19282cfcfc6fee1b95
Elvira <40444030+Ellie1994@users.noreply.github.com>
2019-09-29 20:08:43 +0200

    Update sigmoid.py

--- implementations/sigmoid.py
+++ implementations/sigmoid.py
@@ -7,7 +7,7 @@ def sigmoid(num):
     return 1 / (1 + np.exp(-num))
 
 num_seq = np.linspace(-5, 5, 100)   # 'np.linspace' creates evenly spaced numbers in sequences structured as a numpy array
-plt.plot(num_seq, sigmoid(num_seq),'r') # the 'r'-parameter for 'red. Chose 'g' for 'green' or 'b' for 'blue', etc.
+plt.plot(num_seq, sigmoid(num_seq),'r') # the 'r'-parameter for 'red'. Choose 'g' for 'green' or 'b' for 'blue', etc.
 
 plt.xlabel('x')
 plt.ylabel('y')
